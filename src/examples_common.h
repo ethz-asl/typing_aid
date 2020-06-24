e4687f0429353c11270dfdc68f229abca4ec0a35
Julian Förster <julian.foerster@mavt.ethz.ch>
2020-06-24 13:17:03 +0200

    Ran auto indent

--- src/examples_common.h
+++ src/examples_common.h
@@ -21,24 +21,25 @@
  *
  * @param[in] robot Robot instance to set behavior on.
  */
-void setDefaultBehavior(franka::Robot& robot);
+void setDefaultBehavior(franka::Robot &robot);
 
 /**
  * An example showing how to generate a joint pose motion to a goal position. Adapted from:
  * Wisama Khalil and Etienne Dombre. 2002. Modeling, Identification and Control of Robots
  * (Kogan Page Science Paper edition).
  */
-class MotionGenerator {
- public:
-  /**
+class MotionGenerator
+{
+public:
+    /**
    * Creates a new MotionGenerator instance for a target q.
    *
    * @param[in] speed_factor General speed factor in range [0, 1].
    * @param[in] q_goal Target joint positions.
    */
-  MotionGenerator(double speed_factor, const std::array<double, 7> q_goal);
+    MotionGenerator(double speed_factor, const std::array<double, 7> q_goal);
 
-  /**
+    /**
    * Sends joint position calculations
    *
    * @param[in] robot_state Current state of the robot.
@@ -46,30 +47,30 @@ class MotionGenerator {
    *
    * @return Joint positions for use inside a control loop.
    */
-  franka::JointPositions operator()(const franka::RobotState& robot_state, franka::Duration period);
+    franka::JointPositions operator()(const franka::RobotState &robot_state, franka::Duration period);
 
- private:
-  using Vector7d = Eigen::Matrix<double, 7, 1, Eigen::ColMajor>;
-  using Vector7i = Eigen::Matrix<int, 7, 1, Eigen::ColMajor>;
+private:
+    using Vector7d = Eigen::Matrix<double, 7, 1, Eigen::ColMajor>;
+    using Vector7i = Eigen::Matrix<int, 7, 1, Eigen::ColMajor>;
 
-  bool calculateDesiredValues(double t, Vector7d* delta_q_d) const;
-  void calculateSynchronizedValues();
+    bool calculateDesiredValues(double t, Vector7d *delta_q_d) const;
+    void calculateSynchronizedValues();
 
-  static constexpr double kDeltaQMotionFinished = 1e-6;
-  const Vector7d q_goal_;
+    static constexpr double kDeltaQMotionFinished = 1e-6;
+    const Vector7d q_goal_;
 
-  Vector7d q_start_;
-  Vector7d delta_q_;
+    Vector7d q_start_;
+    Vector7d delta_q_;
 
-  Vector7d dq_max_sync_;
-  Vector7d t_1_sync_;
-  Vector7d t_2_sync_;
-  Vector7d t_f_sync_;
-  Vector7d q_1_;
+    Vector7d dq_max_sync_;
+    Vector7d t_1_sync_;
+    Vector7d t_2_sync_;
+    Vector7d t_f_sync_;
+    Vector7d q_1_;
 
-  double time_ = 0.0;
+    double time_ = 0.0;
 
-  Vector7d dq_max_ = (Vector7d() << 2.0, 2.0, 2.0, 2.0, 2.5, 2.5, 2.5).finished();
-  Vector7d ddq_max_start_ = (Vector7d() << 5, 5, 5, 5, 5, 5, 5).finished();
-  Vector7d ddq_max_goal_ = (Vector7d() << 5, 5, 5, 5, 5, 5, 5).finished();
+    Vector7d dq_max_ = (Vector7d() << 2.0, 2.0, 2.0, 2.0, 2.5, 2.5, 2.5).finished();
+    Vector7d ddq_max_start_ = (Vector7d() << 5, 5, 5, 5, 5, 5, 5).finished();
+    Vector7d ddq_max_goal_ = (Vector7d() << 5, 5, 5, 5, 5, 5, 5).finished();
 };
