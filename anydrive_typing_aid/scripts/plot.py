a1c59ff28b9c4d89978de2c78c432e88f7e17edf
scaro <csauget34@gmail.com>
2021-01-11 11:18:29 +0100

    plot class

--- anydrive_typing_aid/scripts/plot.py
+++ anydrive_typing_aid/scripts/plot.py
@@ -5,10 +5,17 @@ import matplotlib.pyplot as plt
 
 class plot:
     # plot(f, labelx = False) default setting
-    def plot (f,labely):
-        plt.plot(f)
+    def plot(self, x,f,labely):
+        plt.plot(x,f)
         if labely:
             label = input("name of label y")
             plt.ylabel(label)
         plt.xlabel('iter')
         plt.show()
+    
+    def arg(self):
+        x = input("x values")
+        f = input("y values")
+        labely = input("y label")
+        return x, f , labely
+    
