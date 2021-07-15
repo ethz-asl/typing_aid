8cae0dbe2a78a802a69f77d87acb46b4d38b992c
Julian Förster <julian.foerster@mavt.ethz.ch>
2021-07-15 11:44:48 +0200

    Added anydrive config. Created files for ROS package.

---
+++ anydrive_typing_aid/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    version="0.0.0",
+    # scripts=['bin/myscript'],
+    packages=["anydrive_typing_aid"],
+    package_dir={"": "src"},
+)
---
+++ anydrive_typing_aid/src/anydrive_typing_aid/utils/anydrive_interface.py
@@ -0,0 +1,128 @@
+import rospy
+from anydrive_msgs.msg import Reading, Command
+
+
+class AnydriveInterface:
+    def __init__(self):
+        self.prefix = "/anydrive"
+        self.pub_target = rospy.Publisher(
+            self.prefix + "/anydrive/command", Command, queue_size=10
+        )
+
+    def listener(self):
+        msg = rospy.wait_for_message(self.prefix + "/anydrive/reading", Reading)
+        self.joint_position = msg.state.joint_position
+        self.joint_velocity = msg.state.joint_velocity
+        self.joint_torque = msg.state.joint_torque
+        self.motor_current = msg.state.current
+        return (
+            self.joint_torque,
+            self.joint_velocity,
+            self.joint_position,
+            self.motor_current,
+        )
+
+    def callback(self, msg):
+        rospy.loginfo("saved value")
+        self.index = False
+
+    def set_pos(self, param):
+        rospy.Subscriber("lift_arm", Empty, self.callback)
+        rate = rospy.Rate(param["rate"])
+        choice = None
+        # going into control op state
+        fsm.FSM_state().set_FSM_state(4)
+        # starting loop for calibration
+        while choice < 5:
+            choice = input(
+                "Choose param to set  \n 1 = up_lim \n 2 = up_pos \n 3 = down_pos \n 4 = down_lim \n 5 = exit"
+            )
+            if choice < 5:
+                self.index = True
+            while self.index:
+                # sending the desired torque to the drive
+                msg_t = msg_defs.Command()
+                msg_t.mode.mode = np.uint16(10)
+                msg_t.joint_torque = float(0)
+                self.pub_target.publish(msg_t)
+                rate.sleep()
+            msg = rospy.wait_for_message(
+                self.prefix + "/anydrive/reading", msg_defs.Reading
+            )
+            if choice == 1:
+                param["x_end_lim"] = msg.state.joint_position
+            elif choice == 2:
+                param["x_end"] = msg.state.joint_position
+            elif choice == 3:
+                param["x_0"] = msg.state.joint_position
+            elif choice == 4:
+                param["x_0_lim"] = msg.state.joint_position
+
+            msg = msg_defs.Command()
+            msg.mode.mode = 1
+            self.pub_target.publish(msg)
+        return param
+
+    # to change pid gains of motor
+    def pid(self, mode, param):
+        msg = msg_defs.Command()
+        msg.mode.mode = np.uint16(mode)
+        p = param["p"]
+        msg.pid_gains_p = float(p)
+        i = param["i"]
+        msg.pid_gains_i = float(i)
+        d = param["d"]
+        msg.pid_gains_d = float(d)
+        self.pub_target.publish(msg)
+
+    def set_PID(self, param):
+        param["p_gain"] = input("p_gain")
+        param["i_gain"] = input("i_gain")
+        param["d_gain"] = input("d_gain")
+        # initialization of p_error
+        param["p_error"] = input("p_error")
+        return param
+
+    def move(self, mode, position, velocity, torque):
+        msg = msg_defs.Command()
+        msg.mode.mode = np.uint16(mode)
+        if mode in (JOINT_POSITION, 11, 12):
+            msg.joint_position = position
+        if mode in (JOINT_VELOCITY, 11, 12):
+            velocity = float(velocity)
+            msg.motor_velocity = velocity
+            msg.gear_velocity = velocity
+            msg.joint_velocity = velocity
+        if mode in (JOINT_TORQUE, 12):
+            torque = float(torque)
+            msg.joint_torque = torque
+        self.pub_target.publish(msg)
+
+    def lim_check(self, param, t_meas_, p_meas):
+        if p_meas < param["x_0_lim"] or p_meas > param["x_end_lim"]:
+            print("pos_stop")
+            return True
+        if len(t_meas_) < 5:
+            return False
+        if (
+            mean(t_meas_[-5:]) > param["tau_max"]
+            or mean(t_meas_[-5:]) < param["tau_min"]
+        ):
+            print("tau_stop")
+            return True
+
+    # gives the value t_next when the required value is too high or too low
+    def get_lim_val(self, t_meas, position):
+        if t_meas > position["tau_max"]:
+            return position["tau_max"]
+        if t_meas < position["tau_min"]:
+            return position["tau_min"]
+
+    def stop_drive(self):
+        rospy.loginfo("Stopping drive")
+        msg = msg_defs.Command()
+        # freezing the drive
+        msg.mode.mode = 1
+        self.pub_target.publish(msg)
+        rospy.loginfo("Stopped drive")
+
